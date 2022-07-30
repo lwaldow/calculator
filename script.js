c39cbaddfc746e1c042e0696c819ac1b9e3591fa
Waldow <lwaldow2@gmail.com>
2022-07-30 16:52:08 -0700

    Refactor update operand and operator functions

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ const divide = (a, b) => a / b;
 document.querySelectorAll(".button").forEach((elem) => elem.addEventListener("click", receiveInput))
 
 
-let equation = {
-    first: null,
+let eq = {
+    first: "0",
     operator: null,
-    second: null
+    second: "0",
+    current: "first"
 }
 
 function updateOperand(operand) {
-    if(equation.first === null) {
+    if(eq.current === "first") {
+        eq.first = eq.first === "0" ? operand : eq.first + operand;
+    } else {
+        eq.second = eq.second === "0" ? operand : eq.second + operand;
+    }
+    updateDisplay(eq.current);
+}
+
+/*     if(equation.first === null) {
         equation.first = operand;
         updateDisplay("first");
     }
@@ -29,11 +38,21 @@ function updateOperand(operand) {
         equation.second = equation.second + operand;
         updateDisplay("second");
     }
-    
-}
+     */
 
 function updateOperator(operator) {
-    if(equation.first === null) {
+    if(eq.current === "first") {
+        eq.operator = operator;
+        eq.current = "second";
+    } else {
+        eq.first = operate(...eq);
+        eq.operator = operator;
+        eq.second = "0";
+        eq.current = "second";
+    }
+    updateDisplay(eq.current);
+}
+    /* if(equation.first === null) {
         equation.first = 0;
         equation.operator = operator;
     } 
@@ -46,19 +65,31 @@ function updateOperator(operator) {
         equation.second = null;
         updateDisplay("first");
     }
+} */
+
+function evalEquals() {
+    if (equation.first !== null && equation.operator !== null && equation.second !== null) {
+        equation.first = operate(...equation);
+        equation.operator = null;
+        equation.second = null;
+        updateDisplay("first");
+    } else if(equation.first && !equation.operator) {
+        
+    }
 }
 
-
 function operate(a, operator, b) {
-    console.log(operator(a, b));
+    return operator(+a, +b);
 }
 
 function receiveInput(event) {
     let input = event.target.dataset.input;
     switch(input) {
         case "add":
+            updateOperator(add);
             break;
         case "equals":
+            evalEquals();
             break;
         case null:
             let num = event.target.innerText;
